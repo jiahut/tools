def3fafc15ea2b147c97f2eec4be217c78fc6e72
jazz <jiahut@gmail.com>
2012-11-29 21:26:50 +0800

    command line dict

---
+++ command_line_dict/dict.bat
@@ -0,0 +1,3 @@
+@echo off
+rem ruby -S dict.rb %*
+ruby E:\git\win-tools@github\dict.rb %*
---
+++ command_line_dict/dict.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+def check_usage
+	unless ARGV.length == 1
+		puts "Usage: ruby dict.rb word_which_your_want_to_look_up"
+		exit
+	end
+end
+
+def look_up(word)
+	uri = "http://dict.baidu.com/s?wd=#{word}"
+	doc = Nokogiri::HTML(open(URI.escape(uri)))
+	#require 'debug'
+	means = Array.new
+	means.push proc{doc.css('#en-simple-means>div:first>p')},proc{doc.css('#en-net-means li')} #—”≥Ÿº∆À„
+	means.each do |mean|
+	    content = mean.call
+		unless content.empty?
+			content.each do |node|
+				puts node.text
+			end
+			break;
+		end
+	end
+end
+
+if $0 == __FILE__
+  #check_usage
+  look_up ARGV[0]
+end
